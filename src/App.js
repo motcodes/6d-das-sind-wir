6923522af4a6839387bd9d4e331ac15b0b9308eb
Matthias Oberholzer <matthias.m.oberholzer@gmail.com>
2020-10-03 16:30:41 +0200

    second commit

---
+++ src/App.js
@@ -0,0 +1,297 @@
+import React, { useEffect, useRef, useCallback } from 'react';
+import Fullpage, {
+  FullPageSections,
+  FullpageSection,
+} from '@ap.cx/react-fullpage';
+import useWindowSize from './hooks/useWindowSize';
+import './App.scss';
+import { data as people } from './data';
+import { useState } from 'react';
+// import LogoMot from '../public/LogoMot.svg';
+// import LogoFh from '../public/fhlogo.svg';
+
+function App() {
+  //Hook to grab window size
+  const size = useWindowSize();
+
+  // Ref for parent div and scrolling div
+  const app = useRef();
+  const scrollContainer = useRef();
+
+  // Configs
+  const data = {
+    ease: 0.1,
+    current: 0,
+    previous: 0,
+    rounded: 0,
+  };
+
+  // useEffect(() => {
+  //   let vh = window.innerHeight * 0.01;
+  // }, []);
+  // Scrolling
+  const skewScrolling = useCallback(() => {
+    //Set Current to the scroll position amount
+    data.current = window.scrollY;
+    // Set Previous to the scroll previous position
+    data.previous += (data.current - data.previous) * data.ease;
+    // Set rounded to
+    data.rounded = Math.round(data.previous * 100) / 100;
+
+    // Difference between
+    const difference = data.current - data.rounded;
+    const acceleration = difference / size.width;
+    const velocity = +acceleration;
+    const skew = velocity * 7.5;
+    // const skew = velocity * 20;
+    //Assign skew and smooth scrolling to the scroll container
+    // scrollContainer.current.style.transform = `translate3d(0, -${data.rounded}px, 0) skewY(${skew}deg)`;
+
+    //loop vai raf
+    requestAnimationFrame(() => skewScrolling());
+  }, [data, size.width]);
+  // Run scrollrender once page is loaded.
+  useEffect(() => {
+    requestAnimationFrame(() => skewScrolling());
+  }, [skewScrolling]);
+
+  //set the height of the body.
+  useEffect(() => {
+    console.log(scrollContainer.current.clientHeight);
+    setBodyHeight();
+  }, [size.height]);
+
+  //Set the height of the body to the height of the scrolling div
+  const setBodyHeight = () => {
+    // document.body.style.height = `${
+    //   scrollContainer.current.getBoundingClientRect().height
+    // }px`;
+  };
+
+  return (
+    <div ref={app}>
+      {/* <div> */}
+      <Fullpage>
+        <FullPageSections ref={scrollContainer}>
+          <FullpageSection
+            style={{
+              padding: '1rem',
+              height: '100vh',
+              display: 'grid',
+              placeItems: 'center',
+            }}
+          >
+            <h1>
+              6D
+              <br />
+              Das Sind Wir!
+            </h1>
+          </FullpageSection>
+          <FullpageSection
+            style={{
+              padding: '1rem',
+              display: 'grid',
+              placeItems: 'center',
+            }}
+          >
+            <h2 className="mask">
+              wear a<br />
+              mask.
+            </h2>
+          </FullpageSection>
+          <FullpageSection
+            style={{
+              padding: '1rem',
+              display: 'grid',
+              placeItems: 'center',
+            }}
+          >
+            <div className="hero">
+              <ol>
+                <li>Julia Margreiter</li>
+                <li>Luca Mathis</li>
+                <li>Juliane Mohr</li>
+                <li>Renato Mendes Nunes</li>
+                <li>Matthias Oberholzer</li>
+              </ol>
+            </div>
+          </FullpageSection>
+          {people.map(({ name, writtenBy, img, text }) => (
+            <React.Fragment key={name}>
+              <FullpageSection
+                style={{
+                  padding: '1rem',
+                  display: 'grid',
+                  placeItems: 'center',
+                  height: '100vh',
+                }}
+              >
+                <div>
+                  <h2 dangerouslySetInnerHTML={{ __html: name }}></h2>
+                  <p className="writtenBy">beschrieben von {writtenBy}</p>
+                </div>
+              </FullpageSection>
+              <FullpageSection
+                style={{
+                  padding: '1rem',
+                  display: 'grid',
+                  placeItems: 'center',
+                  height: '100vh',
+                }}
+              >
+                {/* <div> */}
+                <img src={img} alt={name} />
+                {/* </div> */}
+              </FullpageSection>
+              <FullpageSection
+                style={{
+                  padding: '1rem',
+                  display: 'grid',
+                  placeItems: 'center',
+                  height: '100vh',
+                }}
+              >
+                <div className="hero">
+                  <p dangerouslySetInnerHTML={{ __html: text }}></p>
+                </div>
+              </FullpageSection>
+            </React.Fragment>
+          ))}
+          <FullpageSection
+            style={{
+              padding: '1rem 2rem 5rem',
+              display: 'grid',
+              placeItems: 'start',
+              height: '80vh',
+            }}
+          >
+            <footer>
+              <p>
+                A website by{' '}
+                <a
+                  href="https://matthiasoberholzer.com"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Matthias Oberholzer
+                </a>
+                <br />
+                <small>
+                  <a
+                    href="https://www.instagram.com/matthias.oberholzer/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    ig: @matthias.oberholzer
+                  </a>
+                </small>
+                <br />
+                <small>
+                  <a
+                    href="https://twitter.com/motcodes"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    tw: @motcodes
+                  </a>
+                </small>
+                <br />
+                <small>
+                  <a
+                    href="https://github.com/motcodes"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    gh: @motcodes
+                  </a>
+                </small>
+              </p>
+              <div className="logo-container">
+                <a
+                  href="https://matthiasoberholzer.com"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <img
+                    className="logo"
+                    src="LogoMot.svg"
+                    alt="Matthis Oberholzer Logo"
+                  />
+                </a>
+                <a
+                  href="https://fh-salzburg.ac.at/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <img
+                    className="logo"
+                    src="fhlogo.svg"
+                    alt="FH Salzburg Logo"
+                  />
+                </a>
+              </div>
+            </footer>
+          </FullpageSection>
+        </FullPageSections>
+      </Fullpage>
+      {/* </div> */}
+    </div>
+  );
+}
+
+export default App;
+
+const skeyScroll = ({ app, scrollContainer }) => (
+  <div ref={app} className="App">
+    <div ref={scrollContainer} className="scroll">
+      <div className="hero">
+        <h1>
+          6D
+          <br />
+          Das Sind Wir!
+        </h1>
+      </div>
+      <div className="hero">
+        <h2 className="mask">
+          wear a<br />
+          mask.
+        </h2>
+      </div>
+      <div className="hero">
+        <ol>
+          <li>Julia Margreiter</li>
+          <li>Luca Mathis</li>
+          <li>Juliane Mohr</li>
+          <li>Renato Mendes Nunes</li>
+          <li>Matthias Oberholzer</li>
+        </ol>
+      </div>
+      {/* {images.map((image, index) => ( */}
+      {/* <React.Fragment key={index}> */}
+      <div className="hero last-bottom">
+        <h2>
+          Matthias
+          <br />
+          Oberholzer
+        </h2>
+        <p className="writtenBy">beschrieben von Matthias Oberholzer</p>
+      </div>
+      <div className="container">
+        <img src={'https://source.unsplash.com/nOT-ZGevv_4'} alt={`people`} />
+      </div>
+      <div className="container last-top">
+        <p>
+          My name is Matthias M. Oberholzer. I currently live in Schleedorf, a
+          small village in Austria, not to be confused with Australia. I visited
+          the department of electronics and technical informatics at the higher
+          technical education institute of Salzburg, where I graduated in 2019.
+          Midway thru my third school year, I started to develop a passion for
+          drawing &amp; designing things. Later on for photography too.
+        </p>
+      </div>
+      <hr />
+      {/* </React.Fragment> */}
+      {/* ))} */}
+    </div>
+  </div>
+);
---
+++ src/data.js
@@ -0,0 +1,43 @@
+export const data = [
+  {
+    name: 'Julia Margreiter',
+    writtenBy: 'Matthias Oberholzer',
+    img:
+      'https://images.unsplash.com/photo-1551392505-f4056032826e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1497&q=80',
+    text: `Julia ist 20 Jahre alt und stammt aus Tirol. Ihre Matura absolvierte sie auf der Innsbrucker Ferrarischule, mit dem Schwerpunkt Mediendesign.<br/>Nach der Schule machte Julia für ein freiwilliges sozial Jahr beim Roten Kreuz. Also falls mal wer im Unterricht ohnmächtig wird, weiß Julia gleich was zu tun ist. 
+<br/>Julia entschied sich für die Vertiefung Film, drauf fragte ich sie, welche 3 Filme aus 3 verschiednen Genre sie mir empfehlen kann, die noch vor 2000 erschienen sind: Psycho (zwecks Spannungsaufbau), Forrest Gump (Schauspielleistung) und Pretty Women, weil sie das Konzept des Filmes so toll findet.<br/>Außerhalb vom Studieren und Menschen retten, liest sie gerne, eines ihrer Lieblingsbücher ist “Das Karussell”, sie hört gerne Musik und wenn zum Fortgehen wird, schmeißt Julia immer “cant’t hold us” rein.<br/>Durch die jetzige Situation ist es ja leider nicht möglich, aber auf Julias Bucketlist steht, dass sie unbedingt eine Spanienrundreise machen will. Sie ist fasziniert von der Sprache und den Landschaften.`,
+  },
+  {
+    name: 'Luca Mathis',
+    writtenBy: 'Julia Margreiter',
+    img:
+      'https://images.unsplash.com/photo-1551392505-f4056032826e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1497&q=80',
+    text: `Julia ist 20 Jahre alt und stammt aus Tirol. Ihre Matura absolvierte sie auf der Innsbrucker Ferrarischule, mit dem Schwerpunkt Mediendesign. Nach der Schule entschied sich Julia für ein freiwilliges sozial Jahr, kurz FSJ, beim Roten Kreuz. Also falls mal wer im Unterricht ohnmächtig wird, weiß Julia gleich was zu tun ist. 
+Julia entschied sich für die Vertiefung Film, drauf fragte ich sie, welche 3 Filme aus 3 verschiednen Genre sie mir empfehlen kann, die noch vor 2000 erschienen sind: Psycho (zwecks Spannungsaufbau), Forrest Gump (Schauspielleistung) und Pretty Women, weil sie das Konzept des Filmes so toll findet.Außerhalb vom Studieren und Menschen retten, liest sie gerne, eines ihrer Lieblingsbücher ist “Das Karussell”, sie hört gerne Musik und wenn zum Fortgehen wird, schmeißt Julia immer “cant’t hold us” rein.Durch die jetzige Situation ist es ja leider nicht möglich, aber auf Julias Bucketlist steht, dass sie unbedingt eine Spanienrundreise machen will. Sie ist fasziniert von der Sprache und den Landschaften.`,
+  },
+  {
+    name: 'Juliane Mohr',
+    writtenBy: 'Luca Mathis',
+    img: 'mohr.jpg',
+    text: `Julia ist 20 Jahre alt und stammt aus Tirol. Ihre Matura absolvierte sie auf der Innsbrucker Ferrarischule, mit dem Schwerpunkt Mediendesign. Nach der Schule entschied sich Julia für ein freiwilliges sozial Jahr, kurz FSJ, beim Roten Kreuz. Also falls mal wer im Unterricht ohnmächtig wird, weiß Julia gleich was zu tun ist. 
+Julia entschied sich für die Vertiefung Film, drauf fragte ich sie, welche 3 Filme aus 3 verschiednen Genre sie mir empfehlen kann, die noch vor 2000 erschienen sind: Psycho (zwecks Spannungsaufbau), Forrest Gump (Schauspielleistung) und Pretty Women, weil sie das Konzept des Filmes so toll findet.Außerhalb vom Studieren und Menschen retten, liest sie gerne, eines ihrer Lieblingsbücher ist “Das Karussell”, sie hört gerne Musik und wenn zum Fortgehen wird, schmeißt Julia immer “cant’t hold us” rein.Durch die jetzige Situation ist es ja leider nicht möglich, aber auf Julias Bucketlist steht, dass sie unbedingt eine Spanienrundreise machen will. Sie ist fasziniert von der Sprache und den Landschaften.`,
+  },
+  {
+    name: 'Renato Mendes Nunes',
+    writtenBy: 'Juliane Mohr',
+    img:
+      'https://images.unsplash.com/photo-1551392505-f4056032826e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1497&q=80',
+    text: `Renato ist 23 Jahre alt und kommt aus Deutschland, weshalb er so manchen österreichischen Dialekt nicht ganz versteht. Sein Abitur hat er auf einem Wirtschaftsgymnasium abgelegt und nun studiert er an der FH Salzburg. Da er sich sehr für die Kunst hinter Videospielen und Filmen interessiert, hat er den Studiengang MMA-Mediendesign gewählt.Die Architektur, Kultur, Kunst und Ästhetik von Japan begeisterten ihn schon immer, daher entschied er sich das Jahr 2019 dort zu verbringen.Zu seinen Hobbys zählen Zeichnen, Joggen und natürlich auch das Spielen von Computerspielen. Bisher kann ich über ihn sagen, dass er ein netter und lustiger Mensch ist, mit dem man sich sehr gut unterhalten kann.
+
+ 
+
+ `,
+  },
+  {
+    name: 'Matthias Oberholzer',
+    writtenBy: 'Renato Mendes Nunes',
+    img: 'oberholzer.jpg',
+    text: `Julia ist 20 Jahre alt und stammt aus Tirol. Ihre Matura absolvierte sie auf der Innsbrucker Ferrarischule, mit dem Schwerpunkt Mediendesign. Nach der Schule entschied sich Julia für ein freiwilliges sozial Jahr, kurz FSJ, beim Roten Kreuz. Also falls mal wer im Unterricht ohnmächtig wird, weiß Julia gleich was zu tun ist. 
+Julia entschied sich für die Vertiefung Film, drauf fragte ich sie, welche 3 Filme aus 3 verschiednen Genre sie mir empfehlen kann, die noch vor 2000 erschienen sind: Psycho (zwecks Spannungsaufbau), Forrest Gump (Schauspielleistung) und Pretty Women, weil sie das Konzept des Filmes so toll findet.Außerhalb vom Studieren und Menschen retten, liest sie gerne, eines ihrer Lieblingsbücher ist “Das Karussell”, sie hört gerne Musik und wenn zum Fortgehen wird, schmeißt Julia immer “cant’t hold us” rein.Durch die jetzige Situation ist es ja leider nicht möglich, aber auf Julias Bucketlist steht, dass sie unbedingt eine Spanienrundreise machen will. Sie ist fasziniert von der Sprache und den Landschaften.`,
+  },
+];
