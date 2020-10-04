2e1bbe901c99a7c6fedde889a787320a85de5282
Matthias Oberholzer <matthias.m.oberholzer@gmail.com>
2020-10-04 22:58:37 +0200

    added luca

--- src/data.js
+++ src/data.js
@@ -2,16 +2,14 @@ export const data = [
   {
     name: 'Julia<br/>Margreiter',
     writtenBy: 'Matthias Oberholzer',
-    img:
-      'https://images.unsplash.com/photo-1551392505-f4056032826e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1497&q=80',
+    img: 'margreiter.jpg',
     text: `Julia ist 20 Jahre alt und stammt aus Tirol. Ihre Matura absolvierte sie auf der Innsbrucker Ferrarischule, mit dem Schwerpunkt Mediendesign.<br/>Nach der Schule machte Julia für ein freiwilliges sozial Jahr beim Roten Kreuz. Also falls mal wer im Unterricht ohnmächtig wird, weiß Julia gleich was zu tun ist. 
 <br/>Julia entschied sich für die Vertiefung Film, drauf fragte ich sie, welche 3 Filme aus 3 verschiednen Genre sie mir empfehlen kann, die noch vor 2000 erschienen sind: Psycho (zwecks Spannungsaufbau), Forrest Gump (Schauspielleistung) und Pretty Women, weil sie das Konzept des Filmes so toll findet.<br/>Außerhalb vom Studieren und Menschen retten, liest sie gerne, eines ihrer Lieblingsbücher ist “Das Karussell”, sie hört gerne Musik und wenn zum Fortgehen wird, schmeißt Julia immer “cant’t hold us” rein.<br/>Durch die jetzige Situation ist es ja leider nicht möglich, aber auf Julias Bucketlist steht, dass sie unbedingt eine Spanienrundreise machen will. Sie ist fasziniert von der Sprache und den Landschaften.`,
   },
   {
-    name: 'Luca<br/>Mathis',
+    name: 'Luca<br/>Mathies',
     writtenBy: 'Julia Margreiter',
-    img:
-      'https://images.unsplash.com/photo-1551392505-f4056032826e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1497&q=80',
+    img: 'mathies.jpg',
     text: `Luca Mathies (der Typ mit dem CD-Laufwerk)<br/>
 Keck, Elegant, Sensibel – mit diesen 3 Wörtern beschreibt ihn seine Freundin.<br/>
 Wir haben ihn alle bis jetzt sehr ehrgeizig kennengelernt, vor allem was seinen Bereich Film angeht. Woher dieser Ehrgeiz kommt ist ganz eindeutig, denn er wusste schon vor seinem Maturaabschluss, dass sein Ziel das MMA Studium ist. Als Kind war der Traumberuf noch Architekt, denn da macht man die große Kohle – zumindest hat ihm das seine Mutter so gesagt. Doch davon ist er offensichtlich schnell abgekommen – zum Glück für uns, sonst hätten wir ihn jetzt nicht kennengelernt :D.<br />
@@ -21,10 +19,12 @@ Seine beinahe Blindheit erträgt er außerdem nur mit Rockmusik und einem Whiske
   },
   {
     name: 'Juliane<br/>Mohr',
-    writtenBy: 'Luca Mathis',
+    writtenBy: 'Luca Mathies',
     img: 'mohr.jpg',
-    text: `Julia ist 20 Jahre alt und stammt aus Tirol. Ihre Matura absolvierte sie auf der Innsbrucker Ferrarischule, mit dem Schwerpunkt Mediendesign. Nach der Schule entschied sich Julia für ein freiwilliges sozial Jahr, kurz FSJ, beim Roten Kreuz. Also falls mal wer im Unterricht ohnmächtig wird, weiß Julia gleich was zu tun ist. 
-Julia entschied sich für die Vertiefung Film, drauf fragte ich sie, welche 3 Filme aus 3 verschiednen Genre sie mir empfehlen kann, die noch vor 2000 erschienen sind: Psycho (zwecks Spannungsaufbau), Forrest Gump (Schauspielleistung) und Pretty Women, weil sie das Konzept des Filmes so toll findet.Außerhalb vom Studieren und Menschen retten, liest sie gerne, eines ihrer Lieblingsbücher ist “Das Karussell”, sie hört gerne Musik und wenn zum Fortgehen wird, schmeißt Julia immer “cant’t hold us” rein.Durch die jetzige Situation ist es ja leider nicht möglich, aber auf Julias Bucketlist steht, dass sie unbedingt eine Spanienrundreise machen will. Sie ist fasziniert von der Sprache und den Landschaften.`,
+    text: `Wenn man Juliane begegnet fällt einem als erstes ihre wunderbar fröhliche und offenherzige Art auf. Nicht erschrecken sollte man sich bei ihrer Größe, denn gleich als zweites fällt einem auf, dass sie wirklich groß ist. In Zahlen 1.81 m.<br/>
+Nur weil jemand in einem kleinen Dorf wohnt heißt es nicht, dass er_sie nicht gerne die Welt erkundet. So reist auch Juliane gerne, welche gerade in Henndorf am Wallersee nähe Salzburg wohnt. Der schönste Ort an dem sie jemals war ist Island, was sie als magisch beschreibt.<br />
+Juliane studiert im MultiMediaTechnologie im ersten Semester, davor machte sie ihre Matura an der HLW in Neumarkt. Apropos Matura: Juliane kennt sich wirklich gut mit Wein und Bellini aus. Wie das jetzt aber zueinander in Verbindung miteinander steht soll mal einfach so stehen gelassen werden.<br/>
+Damit sei gesagt: Auf eine neue Ära mit lustige und aufregende Trinkgeschichten.`,
   },
   {
     name: 'Renato<br/>Mendes Nunes',
